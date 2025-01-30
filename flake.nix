8b2967fa8b4916b4d1d26f7a10a59cbe3b6ad260
yycholla <hanway.colin@gmail.com>
2025-01-29 21:04:01 -0700

    31 current  2025-01-29 20:55:37  25.05.20250127.9db2696     6.12.11                          *

--- flake.nix
+++ flake.nix
@@ -6,6 +6,14 @@
     catppuccin.url = "github:catppuccin/nix";
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+    # obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
+
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.obsidian-nvim.follows = "obsidian-nvim";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,8 +26,17 @@
     home-manager,
     zen-browser,
     hyprpanel,
+    nvf,
+    #obsidian-nvim,
     ...
   } @ inputs: {
+    packages."x86_64-linux".default =
+      (nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        modules = [./nvf-configurations.nix];
+      })
+      .neovim;
+
     nixosConfigurations.yycholla-nix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
@@ -28,7 +45,6 @@
         ./configuration.nix
         catppuccin.nixosModules.catppuccin
         {nixpkgs.overlays = [inputs.hyprpanel.overlay];}
-        home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.backupFileExtension = "HMBackup";
