a073cb7f92afa7a3212c096b334ca170f5483fbf
yycholla <hanway.colin@gmail.com>
2025-01-30 23:10:27 -0700

    start.sh-desktop start wayvnc headless with hyprland

--- dotfiles/hypr/start.sh
+++ dotfiles/hypr/start.sh
@@ -9,3 +9,12 @@ swww-daemon &
 systemctl --user enable --now waybar.service &
 # find bar app
 waybar &
+
+if ["$HOSTNAME" = "yycholla-nixd"]; then
+  echo "Starting wayvnc-headless"
+  hyprctl output create headless vnc
+  wayvnc 0.0.0.0 &
+  wayvncctl output set "vnc"
+else
+  echo "Not on desktop, skipping wayvnc-headless"
+fi
