325918b58950c344ff96443157f2916a689d98e8
yycholla <hanway.colin@gmail.com>
2025-01-30 19:53:36 -0700

    getting somewhere

---
+++ dotfiles/hypr/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Init wallpaper damon
+swww-daemon &
+# Set Wallpaper
+./swww/randomize.sh ~/Wallpapers/
+# Network manager (Install by adding pkgs.networkmanagerapplet to packages)
+# nm-applet --indicator &
+systemctl --user enable --now waybar.service
+# find bar app
+waybar
