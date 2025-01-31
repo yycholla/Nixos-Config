5df5afa888e6d0123f2765d7e6f5107037bf236b
yycholla <hanway.colin@gmail.com>
2025-01-30 21:17:40 -0700

    Sweet release 2

--- dotfiles/hypr/start.sh
+++ dotfiles/hypr/start.sh
@@ -3,9 +3,9 @@
 # Init wallpaper damon
 swww-daemon &
 # Set Wallpaper
-~/Nixos-Config/dotfiles/hypr/swww/randomize.sh ~/Wallpapers/
+~/Nixos-Config/dotfiles/hypr/swww/randomize.sh ~/Wallpapers/ &
 # Network manager (Install by adding pkgs.networkmanagerapplet to packages)
 # nm-applet --indicator &
-systemctl --user enable --now waybar.service
+systemctl --user enable --now waybar.service &
 # find bar app
-waybar
+waybar &
