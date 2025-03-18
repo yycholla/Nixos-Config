c5fa94798690f378ccbe66a6c464f983c0ae0626
yycholla <hanway.colin@gmail.com>
2025-03-17 22:10:20 -0600

    update repo

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -54,6 +54,46 @@
 
   # Enable the X11 windowing system.
   services = {
+    adguardhome = {
+      enable = true;
+      openFirewall = true;
+      # settings = {
+      #   http = {
+      #     # You can select any ip and port, just make sure to open firewalls where needed
+      #     address = "127.0.0.1:3003";
+      #   };
+      #   dns = {
+      #     upstream_dns = [
+      #       # Example config with quad9
+      #       "9.9.9.9#dns.quad9.net"
+      #       "149.112.112.112#dns.quad9.net"
+      #       # Uncomment the following to use a local DNS service (e.g. Unbound)
+      #       # Additionally replace the address & port as needed
+      #       # "127.0.0.1:5335"
+      #     ];
+      #   };
+      #   filtering = {
+      #     protection_enabled = true;
+      #     filtering_enabled = true;
+      #
+      #     parental_enabled = false; # Parental control-based DNS requests filtering.
+      #     safe_search = {
+      #       enabled = false; # Enforcing "Safe search" option for search engines, when possible.
+      #     };
+      #   };
+      #   # The following notation uses map
+      #   # to not have to manually create {enabled = true; url = "";} for every filter
+      #   # This is, however, fully optional
+      #   filters =
+      #     map (url: {
+      #       enabled = true;
+      #       url = url;
+      #     }) [
+      #       "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
+      #       "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+      #     ];
+      # };
+    };
     tailscale = {
       enable = true;
     };
@@ -128,6 +168,9 @@
 
   # Install firefox.
   programs = {
+    adb = {
+      enable = true;
+    };
     hyprland = {
       enable = true;
       package = inputs.hyprland.packages."${pkgs.system}".hyprland;
@@ -186,6 +229,8 @@
 
   networking.firewall = {
     enable = true;
+    allowedTCPPorts = [80 3000];
+    allowedUDPPorts = [53];
   };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
